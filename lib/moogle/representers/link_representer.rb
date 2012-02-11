9b6d6c713f01aa241eb2cce0f79b49da7bca77a4
Benjamin Yu <benjaminlyu@gmail.com>
2012-02-11 11:34:38 -0800

    Finish Link and Target representers.
    Details:
    * Needed to add missing properties to Link and Target representers
      that are present in their models.

--- lib/moogle/representers/link_representer.rb
+++ lib/moogle/representers/link_representer.rb
@@ -7,6 +7,12 @@ module Moogle
 
     property :kind
     property :id
+    property :receiver_ref
+    property :message_kind
+    property :render_options
+    property :suspended
+    property :created_at
+    property :updated_at
 
     def kind
       'moogle/domain/link'
--- lib/moogle/representers/target_representer.rb
+++ lib/moogle/representers/target_representer.rb
@@ -1,3 +1,4 @@
+require 'active_support/core_ext/string/inflections'
 require 'roar/representer/json'
 
 module Moogle
@@ -7,9 +8,19 @@ module Moogle
 
     property :kind
     property :id
+    property :translated_type, :from => :type
+    property :owner_ref
+    property :options
+    property :created_at
+    property :updated_at
 
     def kind
       'moogle/domain/target'
+    end
+
+    def translated_type
+      return $1.to_s.tableize if /^Moogle::(.+)Target$/ === self.type.to_s
+      return nil
     end
 
   end
