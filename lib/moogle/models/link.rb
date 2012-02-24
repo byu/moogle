6aca9dfb82973725b2a95ed5e6fa771eac26215e
Benjamin Yu <benjaminlyu@gmail.com>
2012-02-24 10:25:53 -0800

    Removing idea that we render differently for each link.

--- lib/moogle/models/link.rb
+++ lib/moogle/models/link.rb
@@ -8,8 +8,6 @@ module Moogle
     property :id, Serial
     property :receiver_ref, String, length: 255
     property :message_kind, String, length: 255
-    property :render_options, Json
-    property :suspended, Boolean, :default => false
     timestamps :at
 
     belongs_to :target
