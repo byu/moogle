b257d45969ff348356fd1bbb78d19fe800ede198
Benjamin Yu <benjaminlyu@gmail.com>
2012-03-14 09:21:52 -0700

    Changes Model's default repository to :moogle_db.
    Details:
    * Explicitly set repository to moogle_db instead of default so we can
      have different databases across different projects living in the
      same process.
    * Not 100% sure, so it may change back at some point.

--- lib/moogle/models/link.rb
+++ lib/moogle/models/link.rb
@@ -5,6 +5,10 @@ module Moogle
   class Link
     include DataMapper::Resource
 
+    def self.default_repository_name
+      :moogle_db
+    end
+
     property :id, Serial
     property :receiver_ref, String, length: 255
     property :message_kind, String, length: 255
--- lib/moogle/models/post_log.rb
+++ lib/moogle/models/post_log.rb
@@ -5,6 +5,10 @@ module Moogle
   class PostLog
     include DataMapper::Resource
 
+    def self.default_repository_name
+      :moogle_db
+    end
+
     property :id, Serial
     property :message_kind, String, length: 255
     property :message_ref, String, length: 255
